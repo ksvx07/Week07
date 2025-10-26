0d0bd5116cb33d5fa3ae09bd29d78a1074835b50
이종은 <94448168+ksvx07@users.noreply.github.com>
2025-10-26 23:06:18 +0900

    Revert "Merge branch 'Develop' into Feat/PlayerControl"
    This reverts commit f6d690ba412c18964f8f2edac33a39ee6672b914, reversing
    changes made to 313f2b351472a03780b9d86f24493d74c7165501.

--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public enum SwitchDirection
 {
     LeftAndRight, // 좌우 방향을 하나로 통합
-    TopToBottom,  // 위에서 아래로
-    BottomToTop  // 아래서 위로
+    TopAndBottom  // 상하 방향을 하나로 통합
 }
 public class Stage : MonoBehaviour
 {
@@ -38,15 +37,10 @@ public class Stage : MonoBehaviour
                 shouldGoNext = player.position.x > transform.position.x;
                 break;
 
-            case SwitchDirection.BottomToTop:
+            case SwitchDirection.TopAndBottom:
                 // 플레이어의 y좌표가 경계의 중심보다 크면 '위쪽'으로 나간 것.
                 // 위쪽을 '다음(Next)' 방향으로 간주합니다.
                 shouldGoNext = player.position.y > transform.position.y;
-                break;
-            case SwitchDirection.TopToBottom:
-                // 플레이어의 y좌표가 경계의 중심보다 크면 '위쪽'으로 나간 것.
-                // 위쪽을 '다음(Next)' 방향으로 간주합니다.
-                shouldGoNext = player.position.y < transform.position.y;
                 break;
 
             default:
