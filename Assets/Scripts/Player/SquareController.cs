6462ac22cedabd220f70e1accf2834bc2ce1b032
이종은 <94448168+ksvx07@users.noreply.github.com>
2025-10-27 13:35:37 +0900

    공중점프 픽스

--- Assets/Scripts/Player/SquareController.cs
+++ Assets/Scripts/Player/SquareController.cs
@@ -134,6 +134,7 @@ public class SquareController : MonoBehaviour, IPlayerController
         inputActions.Player.Dash.performed -= OnDash;
         inputActions.Player.Disable();
         moveInput = Vector2.zero;
+        jumpBufferCounter = -1;
         IsGrounded = false;
         rb.excludeLayers = 0;
     }
@@ -610,6 +611,7 @@ public class SquareController : MonoBehaviour, IPlayerController
         rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
         rb.gravityScale = 0f; // ????? ???? ???
 
+        // 이거필요없잖아 네모로 변신이 될때 적용되는건데 이거 왜만듦
         if (isDashing)
             rb.linearVelocity = Vector2.zero;
         else
